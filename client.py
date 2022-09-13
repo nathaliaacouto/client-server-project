21b1d927b3028b700237863d3a2eb84205bcda26
Luiz Fernando <Luizfpbp@gmail.com>
2022-09-13 09:17:50 -0300

    Client trial/Erro

--- client.py
+++ client.py
@@ -2,7 +2,13 @@ import socket
 
 host = str(input(": "))
 port = int(input(": "))
-
 h_p = (host, port)
-conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-conn.connect(h_p)
+
+try:
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn.connect(h_p)
+
+except:
+    print("Não foi possivel conectar no servidor!")
+    
+
