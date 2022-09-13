7bdcd9ad9089c5b761041802ba4b428c3e5b1165
unknown <nvc@cesar.school>
2022-09-13 09:41:27 -0300

    server recieve message nonstop

--- client.py
+++ client.py
@@ -9,10 +9,6 @@ try:
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn.connect(h_p)
 
-<<<<<<< HEAD
-except:
-    print("Não foi possivel conectar no servidor!")
-=======
 except TimeoutError:
     print("Não foi possivel conectar no servidor!")
     sys.exit(0)
@@ -29,4 +25,3 @@ print("Conectado ao servidor!")
 
 
 
->>>>>>> edc73e5ba7d2a1e3466343660c7b846910270d87
