88a13bfa6a5bb575ac4ece421959f5e13a4d37c9
unknown <nvc@cesar.school>
2022-09-13 09:00:36 -0300

    added running server code

--- server.py
+++ server.py
@@ -1,1 +1,20 @@
 import socket
+
+HOST = '127.0.0.1'  #server IP
+PORT = 5000
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Colocando um endereço IP e uma porta no Socket
+host_port = (HOST, PORT)
+tcp.bind(host_port)
+ 
+# Esperando o cliente falar algo
+tcp.listen(1)
+
+print('\nServidor iniciado no IP número', HOST, 'e na porta', PORT)
+print('Servidor ligado, ok\n')
+
+##while True:
+  ##conexao, cliente = tcp.accept()
+  ##print('\nConexão realizada por:', cliente)
